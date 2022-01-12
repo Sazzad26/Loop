afa661fdd7d00d3d7c2ae3fa00207a2fea626b4c
Md Sazzad Hossain <94091024+Sazzad26@users.noreply.github.com>
2022-01-12 16:41:37 +0600

    Add files via upload

---
+++ Divisors of a number.c
@@ -0,0 +1,28 @@
+/*
+An integer N is given. You have to print all the divisors of N. Example : N = 10. the divisors of 10 is 1,2,5,10
+
+NOTE : print all the number in ascending order
+*/
+#include<stdio.h>
+int main()
+{
+    int dividend, divisor=1, count=0;
+    scanf("%d", &dividend);
+        for(int n=divisor; n<=dividend; n++ )
+        {
+            if(dividend%n==0)
+            {
+            count++;
+            }
+        }
+        printf("%d \n", count);
+        for(int n=divisor; n<=dividend; n++ )
+        {
+            if(dividend%n==0)
+            {
+            printf("%d ", n);
+            count++;
+            }
+        }
+    return 0;
+}
