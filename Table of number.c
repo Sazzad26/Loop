1028cec0ff87473f907a8d2be67dd41dbe0a78da
Md Sazzad Hossain <94091024+Sazzad26@users.noreply.github.com>
2022-01-19 02:05:45 +0600

    Add files via upload

---
+++ Table of number.c
@@ -0,0 +1,22 @@
+/* Write a C program to print a table
+of number.
+*/
+
+#include<stdio.h>
+int main()
+{
+    int s,e;
+    scanf("%d %d", &s, &e);
+    for(int i=s; i<=e; i++)
+    {
+        for(int j=s; j<=e; j++)
+        {
+
+            printf(" %d ",i*j);
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}
